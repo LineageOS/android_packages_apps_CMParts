7dc75d1d76f4f4741c58f7ec42cdd2ac35a80a66
Steve Kondik <shade@chemlab.org>
2010-12-29 10:48:16 -0500

    remove overscrolling settings

--- src/com/cyanogenmod/cmparts/activities/UIActivity.java
+++ src/com/cyanogenmod/cmparts/activities/UIActivity.java
@@ -49,8 +49,6 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
     private static final String ROTATION_270_PREF = "pref_rotation_270";
     private static final String RENDER_EFFECT_PREF = "pref_render_effect";
     private static final String POWER_PROMPT_PREF = "power_dialog_prompt";
-    private static final String OVERSCROLL_PREF = "pref_overscroll";
-    private static final String OVERSCROLL_WEIGHT_PREF = "pref_overscroll_weight";
     
     /* Screen Lock */
     private static final String LOCKSCREEN_TIMEOUT_DELAY_PREF = "pref_lockscreen_timeout_delay";
@@ -71,9 +69,6 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
     private CheckBoxPreference mPowerWidgetHideOnChange;
     private Preference mPowerWidgetColor;
     private PreferenceScreen mPowerPicker;
-
-    private CheckBoxPreference mOverscrollPref;
-    private ListPreference mOverscrollWeightPref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -146,15 +141,6 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
         mPowerWidgetHideOnChange.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.EXPANDED_HIDE_ONCHANGE, 0) == 1));
 
-        /* Overscroll */
-        mOverscrollPref = (CheckBoxPreference) prefSet.findPreference(OVERSCROLL_PREF);
-        mOverscrollPref.setChecked(Settings.System.getInt(getContentResolver(), 
-                Settings.System.ALLOW_OVERSCROLL, 0) == 1);
-
-        mOverscrollWeightPref = (ListPreference) prefSet.findPreference(OVERSCROLL_WEIGHT_PREF);
-        int overscrollWeight = Settings.System.getInt(getContentResolver(), Settings.System.OVERSCROLL_WEIGHT, 5);
-        mOverscrollWeightPref.setValue(String.valueOf(overscrollWeight));
-        mOverscrollWeightPref.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
@@ -225,12 +211,6 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
             cp.show();
         }
 
-        if (preference == mOverscrollPref) {
-            value = mOverscrollPref.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.ALLOW_OVERSCROLL, value ? 1 : 0);
-        }
-
         return true;
     }
     
@@ -245,10 +225,6 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
         } else if (preference == mScreenLockScreenOffDelayPref) {
             int screenOffDelay = Integer.valueOf((String)newValue);
             Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_LOCK_SCREENOFF_DELAY, screenOffDelay);
-            return true;
-        } else if (preference == mOverscrollWeightPref) {
-            int overscrollWeight = Integer.valueOf((String)newValue);
-            Settings.System.putInt(getContentResolver(), Settings.System.OVERSCROLL_WEIGHT, overscrollWeight);
             return true;
         }
         return false;
