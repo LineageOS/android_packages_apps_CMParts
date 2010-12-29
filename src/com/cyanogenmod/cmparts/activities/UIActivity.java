87b3846b3cb45af611fda48c9674c51285f7e0d9
Steve Kondik <shade@chemlab.org>
2010-12-29 11:48:28 -0500

    Remove the orientation settings.
    This needs a rewrite in the framework. It appears that Google kanged
    a good portion of our code :)

--- src/com/cyanogenmod/cmparts/activities/UIActivity.java
+++ src/com/cyanogenmod/cmparts/activities/UIActivity.java
@@ -41,9 +41,6 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
     
     /* Other */	
     private static final String PINCH_REFLOW_PREF = "pref_pinch_reflow";
-    private static final String ROTATION_90_PREF = "pref_rotation_90";
-    private static final String ROTATION_180_PREF = "pref_rotation_180";
-    private static final String ROTATION_270_PREF = "pref_rotation_270";
     private static final String RENDER_EFFECT_PREF = "pref_render_effect";
     private static final String POWER_PROMPT_PREF = "power_dialog_prompt";
     
@@ -52,10 +49,7 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
     private static final String LOCKSCREEN_SCREENOFF_DELAY_PREF = "pref_lockscreen_screenoff_delay";
 
     private CheckBoxPreference mPinchReflowPref;
-    private CheckBoxPreference mRotation90Pref;
-    private CheckBoxPreference mRotation180Pref;
-    private CheckBoxPreference mRotation270Pref;
-
+    
     private CheckBoxPreference mPowerPromptPref;
     private ListPreference mRenderEffectPref;
     
@@ -91,16 +85,6 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
                 !getResources().getBoolean(R.bool.has_dual_notification_led)) {
             ((PreferenceCategory)prefSet.findPreference(GENERAL_CATEGORY)).removePreference(mTrackballScreen);
         }
-
-        /* Rotation */
-        mRotation90Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_90_PREF);
-        mRotation180Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_180_PREF);
-        mRotation270Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_270_PREF);
-        int mode = Settings.System.getInt(getContentResolver(),
-                        Settings.System.ACCELEROMETER_ROTATION_MODE, 5);
-        mRotation90Pref.setChecked((mode & 1) != 0);
-        mRotation180Pref.setChecked((mode & 2) != 0);
-        mRotation270Pref.setChecked((mode & 4) != 0);
 
         /* Screen Lock */
         mScreenLockTimeoutDelayPref = (ListPreference) prefSet.findPreference(LOCKSCREEN_TIMEOUT_DELAY_PREF);
@@ -171,17 +155,6 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
             value = mPowerPromptPref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.POWER_DIALOG_PROMPT, value ? 1 : 0);
-        }
-
-        if (preference == mRotation90Pref ||
-            preference == mRotation180Pref ||
-            preference == mRotation270Pref) {
-            int mode = 0;
-            if (mRotation90Pref.isChecked()) mode |= 1;
-            if (mRotation180Pref.isChecked()) mode |= 2;
-            if (mRotation270Pref.isChecked()) mode |= 4;
-            Settings.System.putInt(getContentResolver(),
-                     Settings.System.ACCELEROMETER_ROTATION_MODE, mode);
         }
 
         if(preference == mPowerWidget) {
