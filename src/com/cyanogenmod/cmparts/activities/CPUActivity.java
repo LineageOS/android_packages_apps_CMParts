da27dde9519d8591597bee82bf40c79fca794b5a
NameLess, the Jedi <yoddah@gmail.com>
2011-01-07 23:53:21 +0100

    CPU Settings are now restored after (re)boot unless:  - user unchecks "Set on boot"  - user creates /sdcard/.nocpu file
    Change-Id: I7cbbe0ccb16e388342acc0fdd2470afef36b7b39

--- src/com/cyanogenmod/cmparts/activities/CPUActivity.java
+++ src/com/cyanogenmod/cmparts/activities/CPUActivity.java
@@ -11,7 +11,6 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -22,16 +21,17 @@ import java.lang.Process;
 //
 public class CPUActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
-    private static final String GOV_PREF = "pref_cpu_gov";
-    private static final String GOVERNORS_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors";
-    private static final String GOVERNOR = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";
+    public static final String GOV_PREF = "pref_cpu_gov";
+    public static final String GOVERNORS_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors";
+    public static final String GOVERNOR = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";
 
-    private static final String MIN_FREQ_PREF = "pref_freq_min";
-    private static final String MAX_FREQ_PREF = "pref_freq_max";
-    private static final String FREQ_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies";
-    private static final String FREQ_MAX_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq";
-    private static final String FREQ_MIN_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq";
-    private static final String LOGTAG = "CPUSettings";
+    public static final String MIN_FREQ_PREF = "pref_freq_min";
+    public static final String MAX_FREQ_PREF = "pref_freq_max";
+    public static final String FREQ_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies";
+    public static final String FREQ_MAX_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq";
+    public static final String FREQ_MIN_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq";
+    public static final String SOB_PREF = "pref_set_on_boot";
+    private static final String TAG = "CPUSettings";
     private String GOV_FMT;
     private String MIN_FMT;
     private String MAX_FMT;
@@ -126,31 +126,24 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
                 fname = FREQ_MAX_FILE;
                 //rootcmd = "echo " + (String) newValue + " > " + FREQ_MAX_FILE + "\n";
             }
-            try {
-                FileWriter fw = new FileWriter(fname);
-                try {
-                    fw.write((String) newValue);
-                } finally {
-                    fw.close();
-                }
-            } catch(IOException e) {
-                Log.e(LOGTAG, "Error writing to " + fname + ". Exception: ", e);
-                return false;
-            }
-            if (preference == GovPref) {
-                GovPref.setSummary(String.format(GOV_FMT, (String) newValue));
-            } else if (preference == MinFreqPref) {
-                MinFreqPref.setSummary(String.format(MIN_FMT, MHerzed((String) newValue)));
-            } else if (preference == MaxFreqPref) {
-                MaxFreqPref.setSummary(String.format(MAX_FMT, MHerzed((String) newValue)));
-            }
-            return true;
-        } else {
-            return false;
+
+           if (WriteOneLine(fname, (String) newValue)) {
+               if (preference == GovPref) {
+                   GovPref.setSummary(String.format(GOV_FMT, (String) newValue));
+               } else if (preference == MinFreqPref) {
+                   MinFreqPref.setSummary(String.format(MIN_FMT, MHerzed((String) newValue)));
+               } else if (preference == MaxFreqPref) {
+                   MaxFreqPref.setSummary(String.format(MAX_FMT, MHerzed((String) newValue)));
+               }
+               return true;
+           } else {
+               return false;
+           }
         }
+        return false;
     }
 
-    private String ReadOneLine(String fname) {
+    public static String ReadOneLine(String fname) {
         BufferedReader br;
         String line = null;
 
@@ -162,10 +155,27 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
                 br.close();
             }
         } catch (Exception e) {
-            Log.e(LOGTAG, "IO Exception when reading /sys/ file", e);
+            Log.e(TAG, "IO Exception when reading /sys/ file", e);
         }
         return line;
     }
+
+    public static boolean WriteOneLine(String fname, String value) {
+        try {
+            FileWriter fw = new FileWriter(fname);
+            try {
+                fw.write(value);
+            } finally {
+                fw.close();
+            }
+        } catch(IOException e) {
+            String Error = "Error writing to " + fname + ". Exception: ";
+            Log.e(TAG, Error, e);
+            return false;
+        }
+        return true;
+    }
+
 
     private String MHerzed(String str) {
         String temp;
---
+++ src/com/cyanogenmod/cmparts/services/CPUService.java
@@ -0,0 +1,103 @@
+package com.cyanogenmod.cmparts.services;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.os.IBinder;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.cyanogenmod.cmparts.activities.CPUActivity;
+
+public class CPUService extends Service {
+
+    private static final String UserFile = ".nocpu";
+    public static final String TAG = "CPUSettings";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private SharedPreferences prefs;
+
+    private String Gov;
+    private String MinFreq;
+    private String MaxFreq;
+    private boolean SetOnBoot;
+
+    @Override
+    public void onCreate() {
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        SetOnBoot = prefs.getBoolean(CPUActivity.SOB_PREF, false);
+        if (!SetOnBoot) {
+            Log.i(TAG, "SetOnBoot is false. Exiting.");
+            stopSelf();
+        }
+
+        Gov = prefs.getString(CPUActivity.GOV_PREF, null);
+        MinFreq = prefs.getString(CPUActivity.MIN_FREQ_PREF, null);
+        MaxFreq = prefs.getString(CPUActivity.MAX_FREQ_PREF, null);
+
+        boolean noSettings = (Gov==null) && (MinFreq==null) && (MaxFreq==null) ;
+        if (noSettings) {
+            Log.i(TAG, "No settings saved. Nothing to restore.");
+            stopSelf();
+        }
+
+        // Wait for external storage to settle
+        final Handler hndl = new Handler();
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            public void run() {
+                hndl.post(new Runnable() {
+                    public void run() {
+                        restoreCPU();
+                    }
+                });
+            }
+        }, 120000);
+    }
+
+    private void restoreCPU () {
+
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) ||
+            Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+            String fullPath = Environment.getExternalStorageDirectory().toString() + "/" + UserFile;
+            File file = new File(fullPath);
+            if (file.exists()) {
+                Log.i(TAG, "Nothing to do.");
+                stopSelf();
+            }
+
+            List GovLst = Arrays.asList(
+                    CPUActivity.ReadOneLine(CPUActivity.GOVERNORS_LIST_FILE).split(" "));
+            List FreqLst = Arrays.asList(
+                    CPUActivity.ReadOneLine(CPUActivity.FREQ_LIST_FILE).split(" "));
+
+            if (Gov != null && GovLst.contains(Gov)) {
+                CPUActivity.WriteOneLine(CPUActivity.GOVERNOR, Gov);
+            }
+            if (MaxFreq != null && FreqLst.contains(MaxFreq)) {
+                CPUActivity.WriteOneLine(CPUActivity.FREQ_MAX_FILE, MaxFreq);
+            }
+            if (MinFreq != null && FreqLst.contains(MinFreq)) {
+                CPUActivity.WriteOneLine(CPUActivity.FREQ_MIN_FILE, MinFreq);
+            }
+            Log.i(TAG, "CPU Settings restored.");
+        }
+        stopSelf();
+    }
+}
