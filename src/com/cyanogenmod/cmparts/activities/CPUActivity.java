41d58eb7bf413d6011664698f5d46a75e53e033e
Steve Kondik <shade@chemlab.org>
2011-02-13 03:57:33 -0500

    cmparts: Refactor CPU settings
     * Clean up code to conform to Android style
     * Get rid of unnecessary CPUService
    
    Change-Id: Ibe840a950a394961d52b27eac491c914d74b82ed

--- src/com/cyanogenmod/cmparts/activities/CPUActivity.java
+++ src/com/cyanogenmod/cmparts/activities/CPUActivity.java
@@ -1,3 +1,4 @@
+
 package com.cyanogenmod.cmparts.activities;
 
 import com.cyanogenmod.cmparts.R;
@@ -19,43 +20,45 @@ import java.lang.Process;
 //
 // CPU Related Settings
 //
-public class CPUActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+public class CPUActivity extends PreferenceActivity implements
+        Preference.OnPreferenceChangeListener {
 
     public static final String GOV_PREF = "pref_cpu_gov";
     public static final String GOVERNORS_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors";
     public static final String GOVERNOR = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";
-
     public static final String MIN_FREQ_PREF = "pref_freq_min";
     public static final String MAX_FREQ_PREF = "pref_freq_max";
     public static final String FREQ_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies";
     public static final String FREQ_MAX_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq";
     public static final String FREQ_MIN_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq";
     public static final String SOB_PREF = "pref_set_on_boot";
-    private static final String TAG = "CPUSettings";
-    private String GOV_FMT;
-    private String MIN_FMT;
-    private String MAX_FMT;
 
-    private ListPreference govPref;
-    private ListPreference minFreqPref;
-    private ListPreference maxFreqPref;
+    private static final String TAG = "CPUSettings";
+
+    private String mGovernorFormat;
+    private String mMinFrequencyFormat;
+    private String mMaxFrequencyFormat;
+
+    private ListPreference mGovernorPref;
+    private ListPreference mMinFrequencyPref;
+    private ListPreference mMaxFrequencyPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        GOV_FMT = getString(R.string.cpu_governors_summary);
-        MIN_FMT = getString(R.string.cpu_min_freq_summary);
-        MAX_FMT = getString(R.string.cpu_max_freq_summary);
+        mGovernorFormat = getString(R.string.cpu_governors_summary);
+        mMinFrequencyFormat = getString(R.string.cpu_min_freq_summary);
+        mMaxFrequencyFormat = getString(R.string.cpu_max_freq_summary);
 
-        String[] Governors = readOneLine(GOVERNORS_LIST_FILE).split(" ");
-        String[] FreqValues = readOneLine(FREQ_LIST_FILE).split(" ");
-        String[] Freqs;
+        String[] availableGovernors = readOneLine(GOVERNORS_LIST_FILE).split(" ");
+        String[] availableFrequencies = readOneLine(FREQ_LIST_FILE).split(" ");
+        String[] frequencies;
         String temp;
 
-        Freqs = new String[FreqValues.length];
-        for(int i=0; i <Freqs.length; i++) {
-            Freqs[i] = MHerzed(FreqValues[i]);
+        frequencies = new String[availableFrequencies.length];
+        for (int i = 0; i < frequencies.length; i++) {
+            frequencies[i] = toMHz(availableFrequencies[i]);
         }
 
         setTitle(R.string.cpu_title);
@@ -65,30 +68,30 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
 
         temp = readOneLine(GOVERNOR);
 
-        govPref = (ListPreference) PrefScreen.findPreference(GOV_PREF);
-        govPref.setEntryValues(Governors);
-        govPref.setEntries(Governors);
-        govPref.setValue(temp);
-        govPref.setSummary(String.format(GOV_FMT, temp));
-        govPref.setOnPreferenceChangeListener(this);
+        mGovernorPref = (ListPreference) PrefScreen.findPreference(GOV_PREF);
+        mGovernorPref.setEntryValues(availableGovernors);
+        mGovernorPref.setEntries(availableGovernors);
+        mGovernorPref.setValue(temp);
+        mGovernorPref.setSummary(String.format(mGovernorFormat, temp));
+        mGovernorPref.setOnPreferenceChangeListener(this);
 
         temp = readOneLine(FREQ_MIN_FILE);
 
-        minFreqPref = (ListPreference) PrefScreen.findPreference(MIN_FREQ_PREF);
-        minFreqPref.setEntryValues(FreqValues);
-        minFreqPref.setEntries(Freqs);
-        minFreqPref.setValue(temp);
-        minFreqPref.setSummary(String.format(MIN_FMT, MHerzed(temp)));
-        minFreqPref.setOnPreferenceChangeListener(this);
+        mMinFrequencyPref = (ListPreference) PrefScreen.findPreference(MIN_FREQ_PREF);
+        mMinFrequencyPref.setEntryValues(availableFrequencies);
+        mMinFrequencyPref.setEntries(frequencies);
+        mMinFrequencyPref.setValue(temp);
+        mMinFrequencyPref.setSummary(String.format(mMinFrequencyFormat, toMHz(temp)));
+        mMinFrequencyPref.setOnPreferenceChangeListener(this);
 
         temp = readOneLine(FREQ_MAX_FILE);
 
-        maxFreqPref = (ListPreference) PrefScreen.findPreference(MAX_FREQ_PREF);
-        maxFreqPref.setEntryValues(FreqValues);
-        maxFreqPref.setEntries(Freqs);
-        maxFreqPref.setValue(temp);
-        maxFreqPref.setSummary(String.format(MAX_FMT, MHerzed(temp)));
-        maxFreqPref.setOnPreferenceChangeListener(this);
+        mMaxFrequencyPref = (ListPreference) PrefScreen.findPreference(MAX_FREQ_PREF);
+        mMaxFrequencyPref.setEntryValues(availableFrequencies);
+        mMaxFrequencyPref.setEntries(frequencies);
+        mMaxFrequencyPref.setValue(temp);
+        mMaxFrequencyPref.setSummary(String.format(mMaxFrequencyFormat, toMHz(temp)));
+        mMaxFrequencyPref.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -98,41 +101,43 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
         super.onResume();
 
         temp = readOneLine(FREQ_MAX_FILE);
-        maxFreqPref.setValue(temp);
-        maxFreqPref.setSummary(String.format(MAX_FMT, MHerzed(temp)));
+        mMaxFrequencyPref.setValue(temp);
+        mMaxFrequencyPref.setSummary(String.format(mMaxFrequencyFormat, toMHz(temp)));
 
         temp = readOneLine(FREQ_MIN_FILE);
-        minFreqPref.setValue(temp);
-        minFreqPref.setSummary(String.format(MIN_FMT, MHerzed(temp)));
+        mMinFrequencyPref.setValue(temp);
+        mMinFrequencyPref.setSummary(String.format(mMinFrequencyFormat, toMHz(temp)));
 
         temp = readOneLine(GOVERNOR);
-        govPref.setSummary(String.format(GOV_FMT, temp));
+        mGovernorPref.setSummary(String.format(mGovernorFormat, temp));
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         String fname = "";
 
         if (newValue != null) {
-            if (preference == govPref) {
+            if (preference == mGovernorPref) {
                 fname = GOVERNOR;
-            } else if (preference == minFreqPref) {
+            } else if (preference == mMinFrequencyPref) {
                 fname = FREQ_MIN_FILE;
-            } else if (preference == maxFreqPref) {
+            } else if (preference == mMaxFrequencyPref) {
                 fname = FREQ_MAX_FILE;
             }
 
-           if (writeOneLine(fname, (String) newValue)) {
-               if (preference == govPref) {
-                   govPref.setSummary(String.format(GOV_FMT, (String) newValue));
-               } else if (preference == minFreqPref) {
-                   minFreqPref.setSummary(String.format(MIN_FMT, MHerzed((String) newValue)));
-               } else if (preference == maxFreqPref) {
-                   maxFreqPref.setSummary(String.format(MAX_FMT, MHerzed((String) newValue)));
-               }
-               return true;
-           } else {
-               return false;
-           }
+            if (writeOneLine(fname, (String) newValue)) {
+                if (preference == mGovernorPref) {
+                    mGovernorPref.setSummary(String.format(mGovernorFormat, (String) newValue));
+                } else if (preference == mMinFrequencyPref) {
+                    mMinFrequencyPref.setSummary(String.format(mMinFrequencyFormat,
+                            toMHz((String) newValue)));
+                } else if (preference == mMaxFrequencyPref) {
+                    mMaxFrequencyPref.setSummary(String.format(mMaxFrequencyFormat,
+                            toMHz((String) newValue)));
+                }
+                return true;
+            } else {
+                return false;
+            }
         }
         return false;
     }
@@ -142,7 +147,7 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
         String line = null;
 
         try {
-            br = new BufferedReader (new FileReader(fname), 512);
+            br = new BufferedReader(new FileReader(fname), 512);
             try {
                 line = br.readLine();
             } finally {
@@ -162,7 +167,7 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
             } finally {
                 fw.close();
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             String Error = "Error writing to " + fname + ". Exception: ";
             Log.e(TAG, Error, e);
             return false;
@@ -170,12 +175,7 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
         return true;
     }
 
-
-    private String MHerzed(String str) {
-        String temp;
-
-        temp = str.substring(0, str.length() - 3);
-
-        return (temp + " MHz");
+    private String toMHz(String mhzString) {
+        return new StringBuilder().append(Integer.valueOf(mhzString) / 1000).append(" MHz").toString();
     }
 }
