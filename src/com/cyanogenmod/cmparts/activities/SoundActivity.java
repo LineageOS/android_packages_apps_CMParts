550604b3bc07dee0cf83229afe0bdae88866f99c
mik_os <kiril.mik.os@gmail.com>
2011-05-23 21:14:49 +0300

    Add ability to swap volume keys in selected orientation.

--- src/com/cyanogenmod/cmparts/activities/SoundActivity.java
+++ src/com/cyanogenmod/cmparts/activities/SoundActivity.java
@@ -51,6 +51,8 @@ public class SoundActivity extends PreferenceActivity implements OnPreferenceCha
 
     private static final String VOLUME_CONTROL_SILENT = "vol-ctrl-silent";
 
+    private static final String SWAP_VOLUME_KEYS = "swap-vol-keys";
+
     private static final String VIBRATE_IN_CALL = "vibrate-in-call";
 
     private static final String RINGS_SPEAKER = "ring-speaker";
@@ -136,6 +138,13 @@ public class SoundActivity extends PreferenceActivity implements OnPreferenceCha
         lp.setValue(String.valueOf(SystemProperties.getInt(getKey(ALARMS_LIMITVOL), 1)));
         lp.setSummary(lp.getEntry());
         lp.setOnPreferenceChangeListener(this);
+
+        lp = (ListPreference) prefSet.findPreference(SWAP_VOLUME_KEYS);
+        lp.setValue(String.valueOf(Settings.System.getInt(getContentResolver(),
+                Settings.System.SWAP_VOLUME_KEYS_ORIENTATION,
+                getResources().getInteger(com.android.internal.R.integer.swap_volume_keys_orientation))));
+        lp.setSummary(lp.getEntry());
+        lp.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -146,6 +155,11 @@ public class SoundActivity extends PreferenceActivity implements OnPreferenceCha
         } else if (key.equals(VOLUME_CONTROL_SILENT)) {
             Settings.System.putInt(getContentResolver(), Settings.System.VOLUME_CONTROL_SILENT,
                     getBoolean(newValue) ? 1 : 0);
+        } else if (key.equals(SWAP_VOLUME_KEYS)) {
+            Settings.System.putInt(getContentResolver(), Settings.System.SWAP_VOLUME_KEYS_ORIENTATION,
+                    getInt(newValue));
+            ((AudioManager)getSystemService(Context.AUDIO_SERVICE)).reloadAudioSettings();
+            mHandler.sendMessage(mHandler.obtainMessage(0, key));
         } else if (key.equals(VIBRATE_IN_CALL)) {
             Settings.System.putInt(getContentResolver(), Settings.System.VIBRATE_IN_CALL,
                     getBoolean(newValue) ? 1 : 0);
