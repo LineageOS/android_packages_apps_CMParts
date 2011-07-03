2e71559e53ccf0b1a588dc3e0fd8279dd7d94254
Daniel Tschinder <code@tschinder.de>
2011-07-03 13:02:19 +0200

    Option to disable lockscreen if security screen lock active (2/3)
    [This patch consists of three commits. This one, one for Settings and one for framewoks/base.]
    
    Adds an option to CMParts that allows to turn off lockscreen if
    a security screen lock is enabled.
    
    Default value for new users is "disabled", so the lockscreen is always displayed before
    the security screen.
    
    If you update from an earlier build of CM, this option will be automatically enabled if pattern lock is
    active and otherwise disabled.
    
    Change-Id: I805969599bb50d117b8989d3174f7177f074dc63

--- src/com/cyanogenmod/cmparts/activities/LockscreenActivity.java
+++ src/com/cyanogenmod/cmparts/activities/LockscreenActivity.java
@@ -16,12 +16,23 @@
 
 package com.cyanogenmod.cmparts.activities;
 
+import android.app.admin.DevicePolicyManager;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.cyanogenmod.cmparts.R;
 
 public class LockscreenActivity extends PreferenceActivity {
+
+    private final static String LOCKSCREEN_DISABLE_ON_SECURITY = "pref_lockscreen_disable_on_security";
+
+    private final static String PASSWORD_TYPE_KEY = "lockscreen.password_type";
+
+    private CheckBoxPreference mLockscreenDisableOnSecurity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,5 +40,45 @@ public class LockscreenActivity extends PreferenceActivity {
 
         setTitle(R.string.lockscreen_settings_title_subhead);
         addPreferencesFromResource(R.xml.lockscreen_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLockscreenDisableOnSecurity = (CheckBoxPreference) prefSet
+                .findPreference(LOCKSCREEN_DISABLE_ON_SECURITY);
+
+        int LockscreenDisableOnSecurityValue = Settings.System.getInt(
+                getContentResolver(), Settings.System.LOCKSCREEN_DISABLE_ON_SECURITY, 3);
+
+        if (LockscreenDisableOnSecurityValue == 3) {
+            // We don't have the option set, check if pattern security is
+            // enabled and set the option accordingly
+            final boolean usingLockPattern = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_PATTERN_ENABLED, 0) == 1
+                    && Settings.Secure.getLong(getContentResolver(), PASSWORD_TYPE_KEY,
+                            DevicePolicyManager.PASSWORD_QUALITY_SOMETHING) == DevicePolicyManager.PASSWORD_QUALITY_SOMETHING;
+
+            if (usingLockPattern) {
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_DISABLE_ON_SECURITY, 1);
+                LockscreenDisableOnSecurityValue = 1;
+            } else {
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_DISABLE_ON_SECURITY, 0);
+                LockscreenDisableOnSecurityValue = 0;
+            }
+        }
+        mLockscreenDisableOnSecurity.setChecked(LockscreenDisableOnSecurityValue == 1);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
+        if (preference == mLockscreenDisableOnSecurity) {
+            value = mLockscreenDisableOnSecurity.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_DISABLE_ON_SECURITY, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
