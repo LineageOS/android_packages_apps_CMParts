45d22faa2ba0cbbfdbfba1f2df4f828e7e9f2c82
Xoror <cuongbui@gmail.com>
2011-07-21 19:08:15 +0200

    Code formatting applied and whitespaces removed fix case where newpref could be null Added settings in CMParts for external BT GPS needs patch: I83dd49eb914c24cd182c428d1f7afb90900cf6b1
    Change-Id: I7759199f8ee2aa3a5b781c51590dd9518edae902

--- src/com/cyanogenmod/cmparts/activities/SystemActivity.java
+++ src/com/cyanogenmod/cmparts/activities/SystemActivity.java
@@ -16,21 +16,58 @@
 
 package com.cyanogenmod.cmparts.activities;
 
-import com.cyanogenmod.cmparts.R;
+import java.util.ArrayList;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.os.SystemProperties;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
+import android.provider.Settings;
 
-
-public class SystemActivity extends PreferenceActivity {
+public class SystemActivity extends PreferenceActivity implements
+OnPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setTitle(R.string.system_settings_title_subhead);
         addPreferencesFromResource(R.xml.system_settings);
-        findPreference("changelog").setSummary(getString(R.string.changelog_version) + ": " +
-            SystemProperties.get("ro.modversion", getResources().getString(R.string.changelog_unknown)));
+        findPreference("changelog").setSummary(getString(R.string.changelog_version) 
+                + ": " + SystemProperties.get("ro.modversion", 
+                getResources().getString(R.string.changelog_unknown)));
+        ListPreference btpref = (ListPreference) findPreference("pref_ext_bt_gps");
+        if (btpref != null) {
+            // add known bonded BT devices
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
+                ArrayList<CharSequence> entries = new ArrayList<CharSequence>();
+                entries.add("Internal GPS");
+                ArrayList<CharSequence> values = new ArrayList<CharSequence>();
+                values.add("0");
+                ArrayList<BluetoothDevice> tmp = 
+                    new ArrayList<BluetoothDevice>(mBluetoothAdapter.getBondedDevices());
+                for (BluetoothDevice d : tmp) {
+                    String dname = d.getName() + " - " + d.getAddress();
+                    entries.add(dname);
+                    values.add(d.getAddress());
+                }
+                btpref.setEntries(entries.toArray(new CharSequence[entries.size()]));
+                btpref.setEntryValues(values.toArray(new CharSequence[values.size()]));
+                btpref.setOnPreferenceChangeListener(this);
+            }
+        }
     }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String newPref = (String) newValue;
+        // "0" represents the internal GPS.
+        Settings.System.putString(getContentResolver(), Settings.System.EXTERNAL_GPS_BT_DEVICE,
+                newPref == null ? "0" : newPref);
+        return true;
+    }
+
 }
