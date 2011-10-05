6f886974382ac7d260d72451520be490f8720ffd
m0viefreak <m0viefreak.cm@googlemail.com>
2011-10-05 02:58:24 +0200

    add option to disable volume key beep sounds (1/2)
    Change-Id: Ibaada8b5a0fece80fc8182c44b22a44a433f07a5

--- src/com/cyanogenmod/cmparts/activities/SoundActivity.java
+++ src/com/cyanogenmod/cmparts/activities/SoundActivity.java
@@ -57,6 +57,8 @@ public class SoundActivity extends PreferenceActivity implements OnPreferenceCha
 
     private static final String LOCK_VOLUME_KEYS = "lock-volume-keys";
 
+    private static final String VOLUME_KEY_BEEPS = "volume-key-beeps";
+
     private static final String RINGS_SPEAKER = "ring-speaker";
 
     private static final String RINGS_ATTENUATION = "ring-attn";
@@ -111,6 +113,11 @@ public class SoundActivity extends PreferenceActivity implements OnPreferenceCha
         p = (CheckBoxPreference) prefSet.findPreference(LOCK_VOLUME_KEYS);
         p.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.LOCK_VOLUME_KEYS, 0) != 0);
+        p.setOnPreferenceChangeListener(this);
+
+        p = (CheckBoxPreference) prefSet.findPreference(VOLUME_KEY_BEEPS);
+        p.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.VOLUME_KEY_BEEPS, 1) != 0);
         p.setOnPreferenceChangeListener(this);
 
         p = (CheckBoxPreference) prefSet.findPreference(RINGS_SPEAKER);
@@ -174,6 +181,9 @@ public class SoundActivity extends PreferenceActivity implements OnPreferenceCha
                     getBoolean(newValue) ? 1 : 0);
         } else if (key.equals(LOCK_VOLUME_KEYS)) {
             Settings.System.putInt(getContentResolver(), Settings.System.LOCK_VOLUME_KEYS,
+                    getBoolean(newValue) ? 1 : 0);
+        } else if (key.equals(VOLUME_KEY_BEEPS)) {
+            Settings.System.putInt(getContentResolver(), Settings.System.VOLUME_KEY_BEEPS,
                     getBoolean(newValue) ? 1 : 0);
         } else if (key.equals(NOTIFICATIONS_SPEAKER) || key.equals(RINGS_SPEAKER)
                 || key.equals(ALARMS_SPEAKER)) {
