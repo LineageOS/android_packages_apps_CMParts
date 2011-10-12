7000eda6be991c7164803c2f887cbc21fe35b3d9
Danesh M <daneshm90@gmail.com>
2011-10-12 19:38:56 -0400

    Fix : Lockscreen hide slider toggle
    Update the conditions for "Disable unlock slider" to be active/disactive.
    
    Change-Id: I591dded62d1fe5681c10d9449c34b43e67e84528

--- src/com/cyanogenmod/cmparts/activities/LockscreenUnlockActivity.java
+++ src/com/cyanogenmod/cmparts/activities/LockscreenUnlockActivity.java
@@ -154,6 +154,7 @@ public class LockscreenUnlockActivity extends PreferenceActivity {
             value = mDisableUnlockTab.isChecked();
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.LOCKSCREEN_GESTURES_DISABLE_UNLOCK, value ? 1 : 0);
+            return true;
         }
         return false;
     }
@@ -173,17 +174,21 @@ public class LockscreenUnlockActivity extends PreferenceActivity {
         final File mStoreFile = new File(Environment.getDataDirectory(),
                 "/misc/lockscreen_gestures");
         boolean GestureCanUnlock = false;
+        boolean GestureEnabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_GESTURES_ENABLED, 0) == 1;
         boolean trackCanUnlock = Settings.System.getInt(getContentResolver(),
                 Settings.System.TRACKBALL_UNLOCK_SCREEN, 0) == 1;
         boolean menuCanUnlock = Settings.System.getInt(getContentResolver(),
                 Settings.System.MENU_UNLOCK_SCREEN, 0) == 1;
-        GestureLibrary gl = GestureLibraries.fromFile(mStoreFile);
-        if (gl.load()) {
-            for (String name : gl.getGestureEntries()) {
-                String[] payload = name.split("___", 2);
-                if ("UNLOCK".equals(payload[1])) {
-                    GestureCanUnlock = true;
-                    break;
+        if (GestureEnabled) {
+            GestureLibrary gl = GestureLibraries.fromFile(mStoreFile);
+            if (gl.load()) {
+                for (String name : gl.getGestureEntries()) {
+                    String[] payload = name.split("___", 2);
+                    if ("UNLOCK".equals(payload[1])) {
+                        GestureCanUnlock = true;
+                        break;
+                    }
                 }
             }
         }
