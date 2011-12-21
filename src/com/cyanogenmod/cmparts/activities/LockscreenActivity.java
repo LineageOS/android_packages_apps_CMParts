8d790ccb6e4f3c2589ae41b0de01d636e09f151a
Madison Koenig <pedlar88@gmail.com>
2011-12-21 10:47:59 -0500

    Dock doesn't lockscreen and enables bluetooth (2/2)
    Preferences to disable Lockscreen when Docked.
    
    Idea for this by DeaniusG2x
    
    Change-Id: I159aad08e8d8050f03bb81ec213a04fa2599351a

--- src/com/cyanogenmod/cmparts/activities/LockscreenActivity.java
+++ src/com/cyanogenmod/cmparts/activities/LockscreenActivity.java
@@ -19,6 +19,7 @@ package com.cyanogenmod.cmparts.activities;
 import android.app.admin.DevicePolicyManager;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
@@ -27,11 +28,44 @@ import android.provider.Settings;
 import com.cyanogenmod.cmparts.R;
 
 public class LockscreenActivity extends PreferenceActivity {
+    private static CheckBoxPreference mDockLockPref;
+    private static String DOCK_LOCK_PREF = "pref_dock_nolock";
+
+    private static CheckBoxPreference mDockBluetoothPref;
+    private static String DOCK_BLUETOOTH_PREF = "pref_dock_bluetooth";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setTitle(R.string.lockscreen_settings_title_subhead);
         addPreferencesFromResource(R.xml.lockscreen_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mDockLockPref = (CheckBoxPreference) prefSet.findPreference(DOCK_LOCK_PREF);
+        mDockLockPref.setChecked(Settings.System.getInt(getContentResolver(),
+            Settings.System.DOCK_NOLOCK, 0) == 1);
+        mDockLockPref.setOnPreferenceClickListener(setPrefListener);
+
+        mDockBluetoothPref = (CheckBoxPreference) prefSet.findPreference(DOCK_BLUETOOTH_PREF);
+        mDockBluetoothPref.setChecked(Settings.System.getInt(getContentResolver(),
+            Settings.System.DOCK_BLUETOOTH, 0) == 1);
+        mDockBluetoothPref.setOnPreferenceClickListener(setPrefListener);
     }
+
+    private OnPreferenceClickListener setPrefListener = new OnPreferenceClickListener() {
+        @Override
+        public boolean onPreferenceClick(Preference preference) { 
+            if(preference == mDockLockPref) {
+                boolean value = mDockLockPref.isChecked();
+                Settings.System.putInt(getContentResolver(), Settings.System.DOCK_NOLOCK, value ? 1 : 0);
+                return true;
+            }
+            if(preference == mDockBluetoothPref) {
+                boolean value = mDockBluetoothPref.isChecked();
+                Settings.System.putInt(getContentResolver(), Settings.System.DOCK_BLUETOOTH, value ? 1 : 0);
+                return true;
+            }
+            return false;
+        }
+    }; 
 }
