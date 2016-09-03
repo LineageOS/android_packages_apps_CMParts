9f08818c10c763711296bbc23074a0b943327dac
Steve Kondik <shade@chemlab.org>
2016-09-03 16:24:26 -0700

    cmparts: Add lights settings
     * Converted from Settings app
     * Create a new PartsActivity to launch fragments. This will be improved
       to allow parts to register some metadata which can be queried by
       the Settings app. Ideally we should be able to require nothing
       more than one-liners in the preference XML files inside the Settings
       app for adding new items when complete.
    
    Change-Id: Ic67f65cf2366b32c94bc79cd13492bda159b691f

---
+++ src/org/cyanogenmod/cmparts/PartsActivity.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.cmparts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import org.cyanogenmod.cmparts.notificationlight.*;
+
+public class PartsActivity extends Activity {
+
+    public static final String EXTRA_PART = "part";
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        String part = getIntent().getStringExtra(EXTRA_PART);
+        if (part != null) {
+            SettingsPreferenceFragment fragment = null;
+            if (part.equals("NOTIFICATION_LIGHTS")) {
+                fragment = new NotificationLightSettings();
+            } else if (part.equals("BATTERY_LIGHTS")) {
+                fragment = new BatteryLightSettings();
+            }
+
+            if (fragment != null) {
+                getFragmentManager().beginTransaction()
+                        .replace(android.R.id.content, fragment).commit();
+            }
+        }
+    }
+}
+
