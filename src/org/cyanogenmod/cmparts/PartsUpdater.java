9ac4718b40005ed2dae5762a36f85399f0e975f1
Steve Kondik <steve@cyngn.com>
2016-10-15 01:02:13 -0700

    cmparts: Refactor for RemotePreference and SettingsHelper
     * Use the new stuff in the SDK to standardize on how to deal with
       remote components and settings.
     * Eliminates code and should be more secure.
    
    Change-Id: I9fd148a844877004cd1526a5fc86d3d1529ecbc8

---
+++ src/org/cyanogenmod/cmparts/PartsUpdater.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cyanogenmod.cmparts;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.cyanogenmod.internal.cmparts.PartInfo;
+import org.cyanogenmod.internal.cmparts.PartsList;
+
+import java.lang.reflect.Field;
+import java.util.Objects;
+
+import cyanogenmod.preference.RemotePreferenceUpdater;
+import cyanogenmod.preference.SettingsHelper;
+
+import static cyanogenmod.preference.RemotePreference.EXTRA_KEY;
+import static cyanogenmod.preference.RemotePreference.EXTRA_SUMMARY;
+import static org.cyanogenmod.internal.cmparts.PartsList.EXTRA_PART;
+
+/**
+ * PartsRefresher keeps remote UI clients up to date with any changes in the
+ * state of the Part which should be reflected immediately. For preferences,
+ * the clear use case is refreshing the summary.
+ *
+ * This works in conjunction with CMPartsPreference, which will send an
+ * ordered broadcast requesting updated information. The part will be
+ * looked up, and checked for a static SUMMARY_INFO field. If an
+ * instance of SummaryInfo is found in this field, the result of the
+ * broadcast will be updated with the new information.
+ *
+ * Parts can also call refreshPart to send an asynchronous update to any
+ * active remote components via broadcast.
+ */
+public class PartsUpdater extends RemotePreferenceUpdater {
+
+    private static final String TAG = PartsUpdater.class.getSimpleName();
+
+    public static final String FIELD_NAME_SUMMARY_PROVIDER = "SUMMARY_PROVIDER";
+
+
+    private Refreshable.SummaryProvider getPartSummary(PartInfo pi) {
+        final Class<?> clazz;
+        try {
+            clazz = Class.forName(pi.getFragmentClass());
+        } catch (ClassNotFoundException e) {
+            Log.d(TAG, "Cannot find class: " + pi.getFragmentClass());
+            return null;
+        }
+
+        if (clazz == null || !Refreshable.class.isAssignableFrom(clazz)) {
+            return null;
+        }
+
+        try {
+            final Field f = clazz.getField(FIELD_NAME_SUMMARY_PROVIDER);
+            return (Refreshable.SummaryProvider) f.get(null);
+        } catch (Exception e) {
+            // ignore
+        }
+        return null;
+    }
+
+    @Override
+    protected boolean fillResultExtras(Context context, String key, Bundle bundle) {
+        final PartInfo pi = PartsList.get(context).getPartInfo(key);
+        if (pi == null) {
+            return false;
+        }
+
+        final Refreshable.SummaryProvider si = getPartSummary(pi);
+        if (si == null) {
+            return false;
+        }
+
+        String summary = si.getSummary(context, key);
+
+        if (Objects.equals(summary, pi.getSummary())) {
+            return false;
+        }
+
+        pi.setSummary(si.getSummary(context, key));
+        bundle.putString(EXTRA_KEY, key);
+        bundle.putString(EXTRA_SUMMARY, pi.getSummary());
+        bundle.putParcelable(EXTRA_PART, pi);
+        return true;
+    }
+
+    @Override
+    protected String getSummary(Context context, String key) {
+        return null;
+    }
+
+    public interface Refreshable extends SettingsHelper.OnSettingsChangeListener {
+        public interface SummaryProvider {
+            public String getSummary(Context context, String key);
+        }
+    }
+
+}
