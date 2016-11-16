745491220ec546b810053341fc68ce2c24a8b4ae
Joey Rizzoli <joey@cyanogenmoditalia.it>
2016-11-16 17:10:14 +0100

    CMParts: gestures: add flip2mute
    Change-Id: I0993d8f006369055b4d975af38e79302d86cda4f
    Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>

--- src/org/cyanogenmod/cmparts/input/GesturesSettings.java
+++ src/org/cyanogenmod/cmparts/input/GesturesSettings.java
@@ -16,19 +16,51 @@
 
 package org.cyanogenmod.cmparts.input;
 
+import android.content.Context;
+import android.content.ContentResolver;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
 
 import org.cyanogenmod.cmparts.R;
 import org.cyanogenmod.cmparts.SettingsPreferenceFragment;
 
-public class GesturesSettings extends SettingsPreferenceFragment {
+import cyanogenmod.providers.CMSettings;
+
+public class GesturesSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_FLIP_TO_MUTE = "flip_to_mute_gesture";
+
+    private Context mContext;
+    private SwitchPreference mFlipToMuitePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.gestures_settings);
+
+        mContext = getActivity();
+
+        mFlipToMuitePref = (SwitchPreference) findPreference(KEY_FLIP_TO_MUTE);
+        mFlipToMuitePref.setOnPreferenceChangeListener(this);
+    }
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mFlipToMuitePref) {
+            boolean isFlipOn = (Boolean) newValue;
+            int mValue = isFlipOn ?
+                    CMSettings.Secure.MOTION_BEHAVIOR_FLIP_TO_MUTE_INCOMING_CALL :
+                    CMSettings.Secure.MOTION_BEHAVIOR_NOTHING;
+            CMSettings.Secure.putInt(mContext.getContentResolver(),
+                    CMSettings.Secure.MOTION_BEHAVIOR, mValue);
+            return true;
+        }
+
+        return true;
     }
 
 }
