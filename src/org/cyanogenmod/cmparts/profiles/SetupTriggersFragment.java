6f246a0be1cce06a6a62fc24cbe3a0ffd32429e8
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-16 20:40:29 +0800

    CMParts: Use colorAccent resource instead of hardcoding a color
    Also update the copyrights for the affected files.
    
    Change-Id: I4462f833e47d1b6f2ab3df8c0a5d6edde25115eb

--- src/org/cyanogenmod/cmparts/profiles/SetupTriggersFragment.java
+++ src/org/cyanogenmod/cmparts/profiles/SetupTriggersFragment.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -129,7 +130,7 @@ public class SetupTriggersFragment extends SettingsPreferenceFragment {
         mPager.setAdapter(mAdapter);
 
         PagerTabStrip tabs = (PagerTabStrip) root.findViewById(R.id.tabs);
-        tabs.setTabIndicatorColorResource(R.color.theme_accent);
+        tabs.setTabIndicatorColorResource(android.attr.R.color.colorAccent);
 
         // HACK - https://code.google.com/p/android/issues/detail?id=213359
         ((ViewPager.LayoutParams)tabs.getLayoutParams()).isDecor = true;
