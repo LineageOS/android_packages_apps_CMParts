379e41559062b5d940ee6cef707e988ae8e32e3b
Paul Keith <javelinanddart@gmail.com>
2017-11-23 21:49:38 +0100

    CMParts: gestures: Update KeyHandler for API change
    Change-Id: I634515294995efd66d6e9479fde08c4164097e41

--- src/org/cyanogenmod/cmparts/gestures/KeyHandler.java
+++ src/org/cyanogenmod/cmparts/gestures/KeyHandler.java
@@ -147,10 +147,10 @@ public class KeyHandler implements DeviceKeyHandler {
         }
     }
 
-    public boolean handleKeyEvent(final KeyEvent event) {
+    public KeyEvent handleKeyEvent(final KeyEvent event) {
         final int action = mActionMapping.get(event.getScanCode(), -1);
         if (action < 0 || event.getAction() != KeyEvent.ACTION_UP || !hasSetupCompleted()) {
-            return false;
+            return event;
         }
 
         if (action != 0 && !mEventHandler.hasMessages(GESTURE_REQUEST)) {
@@ -167,7 +167,7 @@ public class KeyHandler implements DeviceKeyHandler {
             }
         }
 
-        return true;
+        return null;
     }
 
     private boolean hasSetupCompleted() {
